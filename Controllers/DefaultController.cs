a3d13e9c5de48819008a50a3a49d76a11004369d
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2022-06-23 20:22:55 +0300

    add testing controller

--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web.Http;
 using System.Web.Http.Cors;
@@ -8,21 +10,109 @@ namespace Documents.Controllers
     [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "GET, POST, PUT, DELETE", SupportsCredentials = true)]
     public class DefaultController : ApiController
     {
+        public class BuildData
+        {
+            public string Message { get; set; }
+            public string ReqstTime { get; set; }
+            public string BuildTime { get; set; }
+        }
+
         [HttpGet]
         [ActionName("list")]
-        public string List()
+        public Dictionary<string, string> BuildInfo()
         {
-            string response = "Server is running. Last build time is ";
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            result.Add("Message", "Server is running");
+            result.Add("Reqst-Time", DateTime.Now.ToString("H:mm:ss - dd MMM. yyyy"));
+
             try
             {
-                response += File.ReadLines(@"C:\Users\iliat\Source\Repos\IliaTrofimov\Documents-backend\bin\BuildInfo.txt")
-                                  .Last().Substring(6);
+                result.Add("Build-Date", DateTime.Parse(
+                    File.ReadLines(@"C:\Users\iliat\Source\Repos\IliaTrofimov\Documents-backend\bin\BuildInfo.txt")
+                   .Last()
+                   .Substring(6).TrimEnd()
+                ).ToString("H:mm:ss - dd MMM. yyyy"));
             }
-            catch (System.Exception ex)
+            catch (Exception e)
             {
-                response += $"unknown (error: {ex.Message})";
-            }           
-            return response;
+                result.Add("Build-Date", "unknown");
+                result["Message"] += $". Cannot get build time ({e.Message})";
+            }
+            return result;
+        }
+
+        [HttpGet]
+        [ActionName("test")]
+        public Dictionary<string, string> Test()
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            Models.DataContext db = new Models.DataContext();
+            result.Add("Build-Date", BuildInfo()["Build-Date"]);
+            
+            try
+            {
+                db.Users.FirstOrDefault();
+                result.Add("Users", "ok");
+            }
+            catch (Exception e) { result.Add("Users", e.Message); }
+
+            try
+            {
+                db.Documents.FirstOrDefault();
+                result.Add("Documents", "ok");
+            }
+            catch (Exception e) { result.Add("Documents", e.Message); }
+
+            try
+            {
+                db.DocumentDataItems.FirstOrDefault();
+                result.Add("DocumentDataItems", "ok");
+            }
+            catch (Exception e) { result.Add("DocumentDataItems", e.Message); }
+
+            try
+            {
+                db.Templates.FirstOrDefault();
+                result.Add("Templates", "ok");
+            }
+            catch (Exception e) { result.Add("Templates", e.Message); }
+
+            try
+            {
+                db.TemplateFields.FirstOrDefault();
+                result.Add("TemplateFields", "ok");
+            }
+            catch (Exception e) { result.Add("TemplateFields", e.Message); }
+
+            try
+            {
+                db.TemplateTables.FirstOrDefault();
+                result.Add("TemplateTables", "ok");
+            }
+            catch (Exception e) { result.Add("TemplateTables", e.Message); }
+
+            try
+            {
+                db.TemplateTypes.FirstOrDefault();
+                result.Add("TemplateTypes", "ok");
+            }
+            catch (Exception e) { result.Add("TemplateTypes", e.Message); }
+
+            try
+            {
+                db.Positions.FirstOrDefault();
+                result.Add("Positions", "ok");
+            }
+            catch (Exception e) { result.Add("Positions", e.Message); }
+
+            try
+            {
+                db.Signs.FirstOrDefault();
+                result.Add("Signs", "ok");
+            }
+            catch (Exception e) { result.Add("Signs", e.Message); }
+
+            return result;
         }
     }
 }
