1deed63561df0b02c3b6ef089b145a419b3c1290
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2022-06-23 14:29:44 +0300

    fixes

--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Http;
+﻿using System.IO;
+using System.Linq;
+using System.Web.Http;
 using System.Web.Http.Cors;
 
 namespace Documents.Controllers
@@ -7,10 +9,20 @@ namespace Documents.Controllers
     public class DefaultController : ApiController
     {
         [HttpGet]
-        [ActionName("index")]
-        public void Index()
+        [ActionName("list")]
+        public string List()
         {
-            Ok("Server is running");
+            string response = "Server is running. Last build time is ";
+            try
+            {
+                response += File.ReadLines(@"C:\Users\iliat\Source\Repos\IliaTrofimov\Documents-backend\bin\BuildInfo.txt")
+                                  .Last().Substring(6);
+            }
+            catch (System.Exception ex)
+            {
+                response += $"unknown (error: {ex.Message})";
+            }           
+            return response;
         }
     }
 }
